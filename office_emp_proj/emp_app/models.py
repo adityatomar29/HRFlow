9f642da85952474cce8f2f0d54ff65d2c9ca74ac
adityatomar29 <aditomar29@gmail.com>
2026-06-26 12:14:49 +0530

    Local Database Update

--- office_emp_proj/emp_app/models.py
+++ office_emp_proj/emp_app/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 class Department(models.Model):
     name = models.CharField(max_length=100, null=False)
     location = models.CharField(max_length=100)
